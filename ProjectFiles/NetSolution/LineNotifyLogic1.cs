c1df4b1e7d3ae6801636604a1868964c7354eda9
phynaro <phynaro@hotmail.com>
2024-05-07 19:25:07 +0700

    improve theme and overall feature

---
+++ ProjectFiles/NetSolution/LineNotifyLogic1.cs
@@ -0,0 +1,30 @@
+#region Using directives
+using System;
+using UAManagedCore;
+using OpcUa = UAManagedCore.OpcUa;
+using FTOptix.HMIProject;
+using FTOptix.NetLogic;
+using FTOptix.Alarm;
+using FTOptix.UI;
+using FTOptix.NativeUI;
+using FTOptix.OPCUAServer;
+using FTOptix.Retentivity;
+using FTOptix.CoreBase;
+using FTOptix.Core;
+using FTOptix.Store;
+using FTOptix.OPCUAClient;
+#endregion
+
+public class LineNotifyLogic1 : BaseNetLogic
+{
+    public override void Start()
+    {
+        // Insert code to be executed when the user-defined logic is started
+    }
+
+    public override void Stop()
+    {
+        // Insert code to be executed when the user-defined logic is stopped
+    }
+ 
+}
